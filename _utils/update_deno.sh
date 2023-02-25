3ffba02907d45a38963b642c91be547653079faf
mjtwelve <21136348+mjtwelve@users.noreply.github.com>
2023-02-25 23:27:54 +0900

    fix github-actions email

--- _utils/update_deno.sh
+++ _utils/update_deno.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 git config user.name "github-actions[bot]"
-git config user.email "github-actions[bot]@users.noreply.github.com"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
 git config pull.rebase false
 
 
